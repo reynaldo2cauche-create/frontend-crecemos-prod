35e690b2b8f38bcd958b2c09853db1f7ef736e0f
lucero <luc4r0.ch4ngr4.99@gmail.com>
2025-12-20 16:35:42 -0500

    se arreglo registro de vacaciones

--- src/pages/rrhh/VacacionesPage.jsx
+++ src/pages/rrhh/VacacionesPage.jsx
@@ -346,7 +346,7 @@ export default function VacacionesPage() {
                           </div>
                         </div>
                       </td>
-                      <td className="px-6 py-4 text-sm text-gray-700">{vac.cargo.nombre}</td>
+                      <td className="px-6 py-4 text-sm text-gray-700">{vac.cargo?.nombre}</td>
                       <td className="px-6 py-4 text-center text-sm font-semibold text-gray-900">{vac.aniosTrabajados}</td>
                       <td className="px-6 py-4 text-center text-sm font-semibold text-gray-900">{vac.diasTotalesAcumulados}</td>
                       <td className="px-6 py-4 text-center text-sm text-gray-600">{vac.diasTomados}</td>
@@ -465,7 +465,7 @@ export default function VacacionesPage() {
                       </div>
                       <div>
                         <p className="text-xs text-gray-500 mb-1">Cargo</p>
-                        <p className="font-semibold text-sm text-gray-900">{modalRegistro.empleado?.cargo}</p>
+                        <p className="font-semibold text-sm text-gray-900">{modalRegistro.empleado?.cargo.nombre}</p>
                       </div>
                       <div>
                         <p className="text-xs text-gray-500 mb-1">Días Disponibles</p>
