b5ed430f8ab691986d84272ac236d814bbd3f316
lucero <luc4r0.ch4ngr4.99@gmail.com>
2025-12-20 16:20:12 -0500

    se avanzo notificaciones

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, createContext, useContext } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { ROLES_NAMES, ROLES } from '../constants/roles';
-// import NotificacionesGlobales from './NotificacionesGlobales';
+import NotificacionesGlobales from './NotificacionesGlobales';
 import {
   CalendarDaysIcon,
   UserGroupIcon,
@@ -195,7 +195,7 @@ const Sidebar = () => {
             {/* Notificaciones - Solo para administradores */}
             {user?.rol?.id === ROLES.ADMINISTRADOR && (
               <div className="flex-shrink-0">
-                {/* <NotificacionesGlobales /> */}
+                <NotificacionesGlobales />
               </div>
             )}
           </div>
